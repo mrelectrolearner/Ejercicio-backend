8a9eb8e79ef2abf6c1fcce509fbcde95c1128afb
luis <luisfelorivas@gmai.com>
2022-07-14 20:09:14 -0500

    ADD utility to get a list from an iterator.

---
+++ src/main/java/org/sofka/demo/utilities/util.java
@@ -0,0 +1,20 @@
+package org.sofka.demo.utilities;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class util {
+    public static <T> List<T> getListFromIterator(Iterator<T> iterator)
+    {
+
+        // Create an empty list
+        List<T> list = new ArrayList<>();
+
+        // Add each element of iterator to the List
+        iterator.forEachRemaining(list::add);
+
+        // Return the List
+        return list;
+    }
+}
